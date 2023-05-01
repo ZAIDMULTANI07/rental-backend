e244338788eb4857ef0a9d0d1bd84efa927004a6
Md. Zaid <md.zaid@Tejbhans-MacBook-Air.local>
2023-05-01 15:22:34 +0530

    fix: authlibservice changed to authapis service

--- src/auth/controllers/auth.ts
+++ src/auth/controllers/auth.ts
@@ -1,13 +1,13 @@
 import { RestController } from "@libs/boat";
 import { Controller, Post, Res } from "@nestjs/common";
-import { AuthLibService } from "../services/auth";
+import { AuthApisService } from "../services/auth";
 import { Dto, Validate } from '@libs/boat/validator';
 import { LoginDto } from "../dto/auth";
 
 @Controller('auth')
 export class AuthController extends RestController {
     constructor(
-        private readonly service: AuthLibService
+        private readonly service: AuthApisService
         ){
         super();
     }
--- src/auth/services/auth.ts
+++ src/auth/services/auth.ts
@@ -1,17 +1,29 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, UnauthorizedException } from "@nestjs/common";
 import { UserLibConstants } from "../constant";
 import { UserRepository } from '../repositories/auth/database';
 import { UserModel } from "@app/user";
 import { LoginDto } from "../dto/auth";
+import { IUser } from "@app/common/interface";
 
 @Injectable()
-export class AuthLibService{
+export class AuthApisService{
     constructor(
         @Inject(UserLibConstants.USER_REPOSITORY)
         private readonly repo: UserRepository
     ){}
 
-    async login(inputs:LoginDto){
-
-    }
+//     async login(inputs: LoginUserDTO): Promise<IUser> {
+    
+//         const user = await this.userLibService.repo
+//           .query()
+//           .findOne({ email })
+//           .whereIn('role', [1, 2]);
+//         if (!user) throw new UnauthorizedException('User does not exists');
+//         const isMatch = await bcrypt.compare(password, user.password);
+//         if (!isMatch) {
+//           throw new UnauthorizedException('Invalid Credentials');
+//         }
+//         const token = await this.jwt.signAsync({ sub: user.id });
+//         return { ...user, token };
+//       }
 }
